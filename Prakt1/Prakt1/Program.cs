328b4ffc8304daafecee50458bdb437fce0132c0
Aryna Shkolna <cxsarina@gmail.com>
2024-01-30 15:03:25 +0200

    Створено файл gitignore та консольний проект з обчисленням суми двох чисел

---
+++ Prakt1/Prakt1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Prakt1
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+          int A = 248;
+          int B = 3343;
+          Console.WriteLine(A + B);
+        }
+    }
+} 
